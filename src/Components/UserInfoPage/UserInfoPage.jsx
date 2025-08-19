45ccf95e340c360d3c6cb255bf6fc0b19e6ce856
bazmo36 <mnalqallaf362@gmail.com>
2025-08-19 10:57:39 +0300

    Display user info sammary

--- src/Components/UserInfoPage/UserInfoPage.jsx
+++ src/Components/UserInfoPage/UserInfoPage.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { getUserInfoDetails } from '../../services/userInfoService'
 
+const activityMultipliers = {
+    sedentary: 1.2,
+    lightly_active: 1.375,
+    moderately_active: 1.55,
+    very_active: 1.725,
+    super_active: 1.9
+}
+
 
 const UserInfoPage = () => {
     return (
         <div>
-
+            <h1>User Info Summary</h1>
+            <p><strong>Age:</strong> {userInfo.age}</p>
+            <p><strong>Gender:</strong> {userInfo.gender}</p>
+            <p><strong>Height:</strong> {userInfo.height.value} {userInfo.height.unit}</p>
+            <p><strong>Weight:</strong> {userInfo.weight.value} {userInfo.weight.unit}</p>
+            <p><strong>Activity Level:</strong> {userInfo.activityLevel}</p>
         </div>
     )
 }
